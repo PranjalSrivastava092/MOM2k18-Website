0ae31c8851860baf570f5bf3dce0295ad71825d8
pranjal <pranjalsri092@gmail.com>
2017-11-25 04:26:04 +0530

    Set debug false

--- mom/settings.py
+++ mom/settings.py
@@ -36,7 +36,7 @@ if os.getcwd() == '/app':
 	ALLOWED_HOSTS = ['*']
 	
 	BASE_DIR=os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
+	STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 	STATICFILES_DIRS = {
 		os.path.join(BASE_DIR, 'static'),
 	}
