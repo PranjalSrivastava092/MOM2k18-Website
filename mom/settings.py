36669cc99aeaf976379a7cea79b6f6fef79506ca
pranjal <pranjalsri092@gmail.com>
2017-11-25 05:15:35 +0530

    moved a folder

--- mom/settings.py
+++ mom/settings.py
@@ -34,12 +34,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 	
 ALLOWED_HOSTS = ['*']
 	
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join(PROJECT_ROOT, 'static'),
+	os.path.join(BASE_DIR, 'static'),
 )
 
 # Application definition
