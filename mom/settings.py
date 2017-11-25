dc1daa5d54ac7d1a86c2358cf48eff4e68ad237f
pranjal <pranjalsri092@gmail.com>
2017-11-25 13:49:12 +0530

    budd

--- mom/settings.py
+++ mom/settings.py
@@ -92,12 +92,7 @@ WSGI_APPLICATION = 'mom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
