5eca38207fa21b314bdf276e7cc2555e934d193e
pranjal <pranjalsri092@gmail.com>
2017-11-26 02:03:26 +0530

    changes

--- mom/settings.py
+++ mom/settings.py
@@ -47,11 +47,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 	
 ALLOWED_HOSTS = ['*']
 	
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'static/'),
 )
 
 # Application definition
